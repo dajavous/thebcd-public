5c5236250df5c4efa29012a0a425b2d2016440e9
dajavous <108701490+dajavous@users.noreply.github.com>
2022-07-05 09:19:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ sheet_name = 'Sheet1'
 df = pd.read_excel(excel_file,
 				sheet_name=sheet_name,
 				usecols='A:E',
-				header=0)
+				header=0,
+		  		index_col=None)
 
 st.dataframe(df, width=1200, height=400)
 
