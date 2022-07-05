2871d47a49456e01bcd4fd8e79088492379c249e
dajavous <108701490+dajavous@users.noreply.github.com>
2022-07-05 08:01:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
-from PIL import Image
 
 st.set_page_config(page_title='Newsletter and Magazine Index')
 st.header('BCD Newsletter and Magazine Index')
