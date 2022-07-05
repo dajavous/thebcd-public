e280c599a1ec4f628be2a8860485ecce4ced7a10
dajavous <108701490+dajavous@users.noreply.github.com>
2022-07-05 12:16:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+import numpy as np
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 excel_file = 'Newsletter-and-Magazine-Index.xlsx'
 sheet_name = 'Sheet1'
@@ -8,6 +10,25 @@ df = pd.read_excel(excel_file,
 				sheet_name=sheet_name,
 				usecols='A:E',
 				header=0)
+
+gb = GridOptionsBuilder.from_dataframe(data)
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+gridOptions = gb.build()
+
+grid_response = AgGrid(
+    data,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='MODEL_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+)
 
 st.dataframe(df)
 
