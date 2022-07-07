cef13c67983e66d22f6f562edb9fc5ab3b1bbc17
dajavous <108701490+dajavous@users.noreply.github.com>
2022-07-07 07:41:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ df = pd.read_excel(excel_file,
 
 gb = GridOptionsBuilder.from_dataframe(df)
 gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
-gb.configure_side_bar() #Add a sidebar
+configure_side_bar(filters_panel= True, columns_panel= False)
 
 gb.configure_column("ISSUE",
                             headerName="ISSUE",
