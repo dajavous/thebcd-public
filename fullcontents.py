d2430a3e9abfa8bee30f79abc97d87d9a8fbf711
dajavous <108701490+dajavous@users.noreply.github.com>
2022-07-21 16:21:50 +0100

    Update fullcontents.py

--- fullcontents.py
+++ fullcontents.py
@@ -13,31 +13,8 @@ df = pd.read_excel(excel_file,
 				   usecols='A:D',
 				   header=0)
 
-with st.expander("Help on using the Index", expanded=True):
-       st.write("""
-	- Using the column headings you can sort by clicking, rearrange columns and change column widths
-	  by clicking and dragging, and remove columns by dragging off the page.
-	- Hover over a column heading and click on the three-bar menu that appears in the column 
-	  heading (or just long press on the heading with a tablet) to open the column search box.
-	- Tap on the table rows anywhere (other than links!) to hide the column search boxes.
-	- Change the theme to provide different colours and font sizes (also resets the table to the starting view).
-	- Refresh the page to go back to the starting view of the table and theme.
-	- Click on "Help on using the Index" above to open or close this help box.
-     """)
-
-hvar = """  <script>
-			var elements = window.parent.document.querySelectorAll('.streamlit-expanderHeader');
-			elements[0].style.fontSize = 'large';
-			elements[0].style.fontWeight = 'bold';
-	    </script>"""
-
-components.html(hvar, height=0, width=0)
-
-available_themes = ["streamlit", "light", "dark", "blue", "fresh", "material"]
-selected_theme = st.selectbox("Choose a different color theme for the table below", available_themes)
-
 gb = GridOptionsBuilder.from_dataframe(df)
-#gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
 #gb.configure_side_bar()
 
 
@@ -51,8 +28,8 @@ grid_response = AgGrid(
     data_return_mode='AS_INPUT', 
     update_mode='MODEL_CHANGED', 
     fit_columns_on_grid_load=False,
-    theme=selected_theme, #Add theme color to the table
+    theme=blue, #Add theme color to the table
     enable_enterprise_modules=False,
-    height=500, 
+    height=650, 
     reload_data=True
 )
