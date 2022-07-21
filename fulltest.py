a9267e71d711c60657e2a32e1688d19352ee400f
dajavous <108701490+dajavous@users.noreply.github.com>
2022-07-21 15:47:30 +0100

    Update fulltest.py

--- fulltest.py
+++ fulltest.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import streamlit as st
+st.set_page_config(layout="wide")
+import streamlit.components.v1 as components
+import numpy as np
+
+st.subheader("Dummy full contents index - testing")
+excel_file = 'melling_mags.xlsx'
+sheet_name = 'sheet1'
+
+df = pd.read_excel(excel_file,
+				   sheet_name=sheet_name,
+				   usecols='A:D',
+				   header=0)
+
+st.dataframe(df)
 
