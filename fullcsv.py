1a741dae886fb60e49b88aa4e349efe3832633a8
dajavous <108701490+dajavous@users.noreply.github.com>
2022-07-22 08:59:09 +0100

    Create fullcsv.py

---
+++ fullcsv.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import streamlit as st
+#st.set_page_config(layout="wide")
+import streamlit.components.v1 as components
+import numpy as np
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
+st.subheader("Dummy Full contents index - please wait for index to load")
+excel_file = 'melling_mags.csv'
+sheet_name = 'sheet1'
+
+df = pd.read_csv(excel_file,
+				   sheet_name=sheet_name,
+				   usecols='A:D',
+				   header=0)
+
+gb = GridOptionsBuilder.from_dataframe(df)
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+#gb.configure_side_bar()
+
+
+
+gridOptions = gb.build()
+
+grid_response = AgGrid(
+    df,
+    gridOptions=gridOptions,
+    allow_unsafe_jscode=True,
+    reload_data=False,
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=False,
+    height=650, 
+)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
